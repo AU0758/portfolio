dcf0cb4a507a6eb2ed8ecd5a530c72255eb75fde
AU0758 <a07582709@gmail.com>
2023-12-10 16:56:00 +0000

    V0.3.1

--- app/(sections)/More/Copywriting/page.tsx
+++ app/(sections)/More/Copywriting/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const CopywriterM = () => {
   return (
-    <div>CopywriterM</div>
+    <main className='h-[100vh] flex items-center'>
+        <p className='absolute left-[-50%]'>IDONTKNOW</p>
+        <p className='absolute right-[-50%] bottom-[0%]'>SOMETINGID</p>
+
+        <h2 className='text-[200px] text-white'><b className='font-normal'>Who</b> sells <b className='font-normal'>the</b> click.</h2>
+    </main >
   )
 }
 
